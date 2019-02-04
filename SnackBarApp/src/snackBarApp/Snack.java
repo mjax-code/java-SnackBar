b8bcda99b600143fbf7bb0cca330a4ecc515ccbb
mjax-code <mjax.code@gmail.com>
2019-02-04 15:11:03 -0800

    Implemented Snack class

--- SnackBarApp/src/snackBarApp/Snack.java
+++ SnackBarApp/src/snackBarApp/Snack.java
@@ -2,6 +2,50 @@ package snackBarApp;
 
 public class Snack
 {
+	private static int maxId = 0;
+	private int id;
 	private String name;
-	private 
+	private int quantity;
+	private double cost;
+	private int vendingMachineId;
+
+	public Snack(String name, int quantity, double cost, int vendingMachineId)
+	{
+		this.name = name;
+		this.quantity = quantity;
+		this.vendingMachineId = vendingMachineId;
+		this.cost = cost;
+		maxId++;
+		id = maxId;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public int getQuantity()
+	{
+		return quantity;
+	}
+
+	public void addQuantity(int amount)
+	{
+		quantity += amount;
+	}
+
+	public void buySnack(int amount)
+	{
+		quantity -= amount;
+	}
+
+	public double getCost(int amount)
+	{
+		return amount * cost;
+	}
 }
