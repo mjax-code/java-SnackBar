9cb5041fa32005314866e2d7bbe564a60f198b4f
mjax-code <mjax.code@gmail.com>
2019-02-04 13:59:13 -0800

    adding app to project repo

---
+++ SnackBarApp/src/snackBarApp/Customer.java
@@ -0,0 +1,37 @@
+package snackBarApp;
+
+public class Customer
+{
+	private static int maxId = 0;
+	private int id; 
+	private String name;
+	private double cashOnHand;
+
+	public Customer(String name, double cashOnHand)
+	{
+		this.name = name;
+		this.cashOnHand = cashOnHand;
+		maxId++;
+		id = maxId;
+	}
+
+	public void addCash(double amount)
+	{
+		cashOnHand += amount;
+	}
+
+	public double getCashOnHand()
+	{
+		return cashOnHand;
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+}
---
+++ SnackBarApp/src/snackBarApp/Snack.java
@@ -0,0 +1,7 @@
+package snackBarApp;
+
+public class Snack
+{
+	private String name;
+	private 
+}
