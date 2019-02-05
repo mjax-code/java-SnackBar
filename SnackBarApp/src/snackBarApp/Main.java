e15c00c4bc0cfa42405a9ac663f879d79e893c21
mjax-code <mjax.code@gmail.com>
2019-02-04 16:48:41 -0800

    finish snack app logic in main

--- SnackBarApp/src/snackBarApp/Main.java
+++ SnackBarApp/src/snackBarApp/Main.java
@@ -15,12 +15,36 @@ public class Main
     Snack chocolateBar = new Snack("Chocolate Bar", 36, 1.00, food.getId());
     Snack pretzel = new Snack("Pretzel", 30, 2.00, food.getId());
     
-    Snack soda = new Snack("Soda", 24, 2.50, drink.getId);
-    Snack water = new Snack("Water", 20, 2.75, drink.getId);
+    Snack soda = new Snack("Soda", 24, 2.50, drink.getId());
+    Snack water = new Snack("Water", 20, 2.75, drink.getId());
 
     System.out.println("***Operations***");
-    System.out.println(food.getId());
-    System.out.println(drink.getId());
+    c1.buySnack(soda.buySnack(3));
+    System.out.println(c1.getCashOnHand());
+    System.out.println(soda.getQuantity());
+
+    c1.buySnack(pretzel.buySnack(1));
+    System.out.println(c1.getCashOnHand());
+    System.out.println(pretzel.getQuantity());
+
+    c2.buySnack(soda.buySnack(2));
+    System.out.println(c2.getCashOnHand());
+    System.out.println(soda.getQuantity());
+
+    c1.addCash(10.00);
+    System.out.println(c1.getCashOnHand());
+
+    c1.buySnack(chocolateBar.buySnack(1));
+    System.out.println(c1.getCashOnHand());
+    System.out.println(chocolateBar.getQuantity());
+
+    pretzel.addQuantity(12);
+    System.out.println(pretzel.getQuantity());
+   
+    c2.buySnack(pretzel.buySnack(3));
+    System.out.println(c2.getCashOnHand());
+    System.out.println(pretzel.getQuantity());
+   
   }
   public static void main(String[] args)
   {
